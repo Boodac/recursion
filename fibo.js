7b1d68fbb81b83114379b2a6e49c76bb491bad7b
boodac <180095661+Boodac@users.noreply.github.com>
2024-12-22 09:08:50 -0500

    Add fibo.js with both iterative and recursive functions.

---
+++ fibo.js
@@ -0,0 +1,20 @@
+function fibn (number) {
+    var arr = new Array();
+    for(let i = 0; i < number; i++) {
+        if(i<2) {
+            arr.push(i);
+        } else {
+            arr.push(Number(arr[i-2]) + Number(arr[i-1]));
+        }
+    }
+    return arr;
+}
+
+function fibsRec(number, current = 0, arr = []) {
+    if(number <= current) { arr[number] = current; return arr; }
+    if(arr.length < 2) arr.push(current);
+    else arr.push(Number(arr[current-2]) + Number(arr[current-1]));
+    return fibsRec(number, ++current, arr);
+}
+
+console.log(fibsRec(8));
